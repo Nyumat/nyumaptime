5ce8b7cf7147086304b9af41381d26b20f731e8e
Thomas <nyumat18@gmail.com>
2024-11-05 04:08:18 -0800

    modularize uptime checker

--- monitor/check.go
+++ monitor/check.go
@@ -2,10 +2,10 @@ package monitor
 
 import (
 	"context"
-	"time"
-
+    "golang.org/x/sync/errgroup"
 	"encore.app/db"
 	"encore.dev/storage/sqldb"
+	"encore.dev/cron"
 )
 
 // Check checks a single site.
@@ -16,6 +16,11 @@ func Check(ctx context.Context, siteID int) error {
 	if err != nil {
 		return err
 	}
+
+	return check(ctx, site)
+}
+
+func check(ctx context.Context, site *db.Site) error {
 	result, err := Ping(ctx, site.URL)
 	if err != nil {
 		return err
@@ -23,17 +28,44 @@ func Check(ctx context.Context, siteID int) error {
 
 	_, err = connection.Exec(ctx,
 		`INSERT INTO checks (site_id, up, checked_at)
-		VALUES ($1, $2, $3)`,
-		siteID, result.Up, time.Now(),
+		VALUES ($1, $2, NOW())`,
+		site.ID, result.Up,
 	)
+
+	return err
+}
+
+
+// CheckAll checks all sites.
+//
+//encore:api public method=POST path=/checkall
+func CheckAll(ctx context.Context) error {
+	// Get all the tracked sites.
+	resp, err := db.List(ctx)
 	if err != nil {
 		return err
 	}
-	return nil
+
+	// Check up to 8 sites concurrently.
+	g, ctx := errgroup.WithContext(ctx)
+	g.SetLimit(8)
+	for _, site := range resp.Sites {
+		site := site // capture for closure
+		g.Go(func() error {
+			return check(ctx, site)
+		})
+	}
+	return g.Wait()
 }
 
 // Define a database named 'checks', using the database migrations
 // in the "./migrations" folder.
 var connection = sqldb.NewDatabase("monitor", sqldb.DatabaseConfig{
 	Migrations: "./migrations",
+})
+
+var _ = cron.NewJob("check-all", cron.JobConfig{
+	Title:    "Check all sites",
+	Endpoint: CheckAll,
+	Every:    1 * cron.Hour,
 })
