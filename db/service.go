eae750304af1a6fa2ae41e9a83457bdeaeb72ae8
Thomas <nyumat18@gmail.com>
2024-11-05 03:00:05 -0800

    init db on startup

---
+++ db/service.go
@@ -0,0 +1,30 @@
+// Service site keeps track of which sites to monitor.
+package site
+
+import (
+	"encore.dev/storage/sqldb"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+//encore:service
+type Service struct {
+	db *gorm.DB
+}
+
+// Define a database named 'site', using the database migrations
+// in the "./migrations" folder.
+var db = sqldb.NewDatabase("site", sqldb.DatabaseConfig{
+	Migrations: "./migrations",
+})
+
+// automatically called on startup.
+func initService() (*Service, error) {
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: db.Stdlib(),
+	}))
+	if err != nil {
+		return nil, err
+	}
+	return &Service{db: db}, nil
+}
