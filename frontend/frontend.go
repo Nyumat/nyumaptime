5a9780c86a352ea6652182f6db141c9bfaa83938
Thomas <nyumat18@gmail.com>
2024-11-05 03:00:20 -0800

    update frontened desc

--- frontend/frontend.go
+++ frontend/frontend.go
@@ -17,10 +17,7 @@ var (
 	handler   = http.StripPrefix("/frontend/", http.FileServer(http.FS(assets)))
 )
 
-// Serve serves the frontend for development.
-// For production use we recommend deploying the frontend
-// using Vercel, Netlify, or similar.
-//
+// Serves the frontend for development
 //encore:api public raw path=/frontend/*path
 func Serve(w http.ResponseWriter, req *http.Request) {
 	if path := encore.CurrentRequest().PathParams.Get("path"); path == "env.js" {
